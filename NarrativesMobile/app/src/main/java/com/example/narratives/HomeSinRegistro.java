30d99e7314357412f0263017afb0ba9bc32644e2
IsmaelMartinez7 <818903@unizar.es>
2024-03-01 10:28:43 +0100

    Agregando continuar como invitado

---
+++ NarrativesMobile/app/src/main/java/com/example/narratives/HomeSinRegistro.java
@@ -0,0 +1,13 @@
+package com.example.narratives;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeSinRegistro extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.homesinregistro);
+        super.onCreate(savedInstanceState);
+    }
+}
--- NarrativesMobile/app/src/main/java/com/example/narratives/LoginActivity.java
+++ NarrativesMobile/app/src/main/java/com/example/narratives/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +14,13 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.inicio_sesion);
         super.onCreate(savedInstanceState);
 
+        TextView invitadoTextView = findViewById(R.id.invitadoTextView);
+        invitadoTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirHomeSinRegistro();
+            }
+        });
         Button botonIniciarSesion = (Button) findViewById(R.id.botonIniciarSesion);
         botonIniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +33,11 @@ public class LoginActivity extends AppCompatActivity {
 
     public void abrirMenuInicio() {
         Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void abrirHomeSinRegistro() {
+        Intent intent = new Intent(this, HomeSinRegistro.class);
         startActivity(intent);
     }
 
