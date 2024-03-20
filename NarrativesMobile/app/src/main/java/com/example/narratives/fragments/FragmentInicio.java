ec366ee39b637ce1be3c91f4f2bdd619ee6bdb0e
IsmaelMartinez7 <818903@unizar.es>
2024-03-20 22:41:43 +0100

    Cambios del lunes de home

--- NarrativesMobile/app/src/main/java/com/example/narratives/fragments/FragmentInicio.java
+++ NarrativesMobile/app/src/main/java/com/example/narratives/fragments/FragmentInicio.java
@@ -8,8 +8,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.narratives.R;
+
+import java.util.ArrayList;
 
 
 public class FragmentInicio extends Fragment {
@@ -21,5 +25,46 @@ public class FragmentInicio extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_inicio, container, false);
+    }
+
+
+    public void setMejoresValorados(){
+
+    }
+
+    public void setRomance() {
+        ImageView ivRomance1, ivRomance2, ivRomance3, ivRomance4;
+        TextView tvRomance1, tvRomance2, tvRomance3, tvRomance4;
+        ArrayList<ImageView> imagenesRomance;
+
+
+        View rootView = getView();
+
+        if (rootView != null) {
+            ivRomance1 = rootView.findViewById(R.id.imageViewRomance1);
+            ivRomance2 = rootView.findViewById(R.id.imageViewRomance2);
+            ivRomance3 = rootView.findViewById(R.id.imageViewRomance3);
+            ivRomance4 = rootView.findViewById(R.id.imageViewRomance4);
+
+            tvRomance1 = rootView.findViewById(R.id.textViewRomance1);
+            tvRomance2 = rootView.findViewById(R.id.textViewRomance2);
+            tvRomance3 = rootView.findViewById(R.id.textViewRomance3);
+            tvRomance4 = rootView.findViewById(R.id.textViewRomance4);
+
+            //CAMBIAR LAS IMAGENES LUEGO POR LA CONSULTA QUE SE REALICE
+            ivRomance1.setImageResource(R.drawable.icono_imagen_estandar_foreground);
+            ivRomance2.setImageResource(R.drawable.icono_imagen_estandar_foreground);
+            ivRomance3.setImageResource(R.drawable.icono_imagen_estandar_foreground);
+            ivRomance4.setImageResource(R.drawable.icono_imagen_estandar_foreground);
+
+            tvRomance1.setText("9.0");
+            tvRomance2.setText("9.0");
+            tvRomance3.setText("9.0");
+            tvRomance4.setText("9.0");
+        }
+    }
+
+    public void setCienciaFiccion(){
+
     }
 }
