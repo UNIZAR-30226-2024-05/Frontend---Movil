b37fe7a0147f8f9f293b81b8e7030d7de40c0355
Manu (839304) <839304@unizar.es>
2024-03-28 21:38:22 +0100

    Pantalla escuchando - reproduce, pausa, resetea y va a un minuto concreto del audio

--- NarrativesMobile/app/src/main/java/com/example/narratives/fragments/FragmentEscuchando.java
+++ NarrativesMobile/app/src/main/java/com/example/narratives/fragments/FragmentEscuchando.java
@@ -1,41 +1,66 @@
 package com.example.narratives.fragments;
 
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.narratives.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class FragmentEscuchando extends Fragment {
+
+    MediaPlayer mediaPlayer;
+
+    FloatingActionButton fabPlay;
+    FloatingActionButton fabPause;
+
+    FloatingActionButton fabReplay;
+
+    FloatingActionButton fabMinutoConcreto;
+
+
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_escuchando, container, false);
-        FloatingActionButton fabPause = (FloatingActionButton) view.findViewById(R.id.botonPauseEscuchando);
-        fabPause.setEnabled(false);
-
         return view;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
 
-        FloatingActionButton fabPlay = (FloatingActionButton) getView().findViewById(R.id.botonPlayEscuchando);
-        FloatingActionButton fabPause = (FloatingActionButton) getView().findViewById(R.id.botonPauseEscuchando);
+        fabPlay = (FloatingActionButton) getView().findViewById(R.id.botonPlayEscuchando);
+        fabPause = (FloatingActionButton) getView().findViewById(R.id.botonPauseEscuchando);
+        fabReplay = (FloatingActionButton) getView().findViewById(R.id.botonReplayEscuchando);
+        fabMinutoConcreto = (FloatingActionButton) getView().findViewById(R.id.botonMinutoConcretoEscuchando);
+
+        fabPause = (FloatingActionButton) view.findViewById(R.id.botonPauseEscuchando);
+        fabPlay = (FloatingActionButton) getView().findViewById(R.id.botonPlayEscuchando);
+        fabPause.setEnabled(false);
+        //fabPlay.setEnabled(false);
+
+
+        mediaPlayer = MediaPlayer.create(getContext(), R.raw.exclusive);
+        //mediaPlayer = MediaPlayer.create(getContext(), R.raw.zowi);
 
         fabPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 fabPause.setEnabled(true);
                 fabPlay.setEnabled(false);
+                mediaPlayer.start();
             }
         });
 
@@ -44,9 +69,64 @@ public class FragmentEscuchando extends Fragment {
             public void onClick(View view) {
                 fabPlay.setEnabled(true);
                 fabPause.setEnabled(false);
+                mediaPlayer.pause();
             }
         });
 
+        fabReplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mediaPlayer.seekTo(0);
+            }
+        });
+
+        fabMinutoConcreto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.seekTo(109000);
+            }
+        });
+
+
+
+
     }
 
+    public void actualizarDuracionAudio(){
+        int duration = mediaPlayer.getDuration();
+        String time = String.format("%02d min, %02d sec",
+                TimeUnit.MILLISECONDS.toMinutes(duration),
+                TimeUnit.MILLISECONDS.toSeconds(duration) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration))
+        );
+
+
+    }
+
+
+    public void prepararAudio(String url){
+        //url es http://......
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioAttributes(
+                new AudioAttributes.Builder()
+                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .build()
+        );
+
+        try{
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.prepareAsync(); // might take long! (for buffering, etc)
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+
+                    fabPlay.setEnabled(true);
+                }
+            });
+        } catch (Exception e){
+            Toast.makeText(getContext(), "Error preparando audio", Toast.LENGTH_LONG).show();
+        }
+    }
 }
