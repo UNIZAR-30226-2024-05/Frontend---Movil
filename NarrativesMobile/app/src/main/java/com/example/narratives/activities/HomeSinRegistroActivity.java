e59a7fe046e83b1fa797d0f7f4ffbea96313212a
IsmaelMartinez7 <818903@unizar.es>
2024-04-11 19:39:21 +0200

    Cambiando home sin inicio parte 1

--- NarrativesMobile/app/src/main/java/com/example/narratives/activities/HomeSinRegistroActivity.java
+++ NarrativesMobile/app/src/main/java/com/example/narratives/activities/HomeSinRegistroActivity.java
@@ -4,12 +4,21 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.transition.TransitionSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.narratives.R;
+import com.example.narratives.informacion.InfoAudiolibros;
+import com.example.narratives.menuprincipal.adaptador;
+import com.example.narratives.peticiones.Audiolibro;
+
+import java.util.ArrayList;
 
 public class HomeSinRegistroActivity extends AppCompatActivity {
     @Override
@@ -20,22 +29,42 @@ public class HomeSinRegistroActivity extends AppCompatActivity {
         setContentView(R.layout.homesinregistro);
         super.onCreate(savedInstanceState);
 
+        obtenerAudiolibrosEjemplo();
+        rv = findViewById(R.id.RecyclerViewRecomendados);
+        rv.setLayoutManager(new LinearLayoutManager(HomeSinRegistroActivity.this, LinearLayoutManager.HORIZONTAL, false));
+        adaptador = new adaptador(HomeSinRegistroActivity.this, audiolibros);
+        rv.setAdapter(adaptador);
 
-        findViewById(R.id.logoHomeSinRegistro).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                abrirMenuMain();
-            }
-        });
+        rvTerror = findViewById(R.id.RecyclerViewTerror1);
+        rvTerror.setLayoutManager(new LinearLayoutManager(HomeSinRegistroActivity.this, LinearLayoutManager.HORIZONTAL, false));
+        rvTerror.setAdapter(adaptador);
 
-        findViewById(R.id.botonIrLoginDesdeInicio).setOnClickListener(new View.OnClickListener() {
+        rvFantasia = findViewById(R.id.RecyclerFantasia1);
+        rvFantasia.setLayoutManager(new LinearLayoutManager(HomeSinRegistroActivity.this, LinearLayoutManager.HORIZONTAL, false));
+        rvFantasia.setAdapter(adaptador);
+
+        rvMitologia = findViewById(R.id.RecyclerMitologia1);
+        rvMitologia.setLayoutManager(new LinearLayoutManager(HomeSinRegistroActivity.this, LinearLayoutManager.HORIZONTAL, false));
+        rvMitologia.setAdapter(adaptador);
+
+        rvPoesia = findViewById(R.id.RecyclerPoesia1);
+        rvPoesia.setLayoutManager(new LinearLayoutManager(HomeSinRegistroActivity.this, LinearLayoutManager.HORIZONTAL, false));
+        rvPoesia.setAdapter(adaptador);
+
+        rvNovela = findViewById(R.id.RecyclerNovela1);
+        rvNovela.setLayoutManager(new LinearLayoutManager(HomeSinRegistroActivity.this, LinearLayoutManager.HORIZONTAL, false));
+        rvNovela.setAdapter(adaptador);
+
+        ;
+
+        findViewById(R.id.menu_sinregistro_1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 abrirMenuLogin();
             }
         });
 
-        findViewById(R.id.botonIrRegistroDesdeInicio).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.menu_sinregistro_2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 abrirMenuRegistro();
@@ -58,4 +87,45 @@ public class HomeSinRegistroActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
+    RecyclerView rv, rvTerror, rvFantasia, rvMitologia, rvNovela, rvPoesia;
+    com.example.narratives.menuprincipal.adaptador adaptador;
+    private ArrayList<Audiolibro> audiolibros;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inicio, container, false);
+    }
+
+
+    private void obtenerAudiolibrosEjemplo(){
+        String[] portadas = {
+                "https://narrativesarchivos.blob.core.windows.net/imagenes/ElCoco.jpg",
+                "https://narrativesarchivos.blob.core.windows.net/imagenes/ElHombreDelTrajeNegro.jpg",
+                "https://narrativesarchivos.blob.core.windows.net/imagenes/ElUmbralDeLaNoche.jpg",
+                "https://narrativesarchivos.blob.core.windows.net/imagenes/HarryPotter1.jpeg",
+                "https://narrativesarchivos.blob.core.windows.net/imagenes/LaOdisea.png",
+                "https://narrativesarchivos.blob.core.windows.net/imagenes/OrgulloYPrejuicio.jpg",
+                "https://narrativesarchivos.blob.core.windows.net/imagenes/Popsy.jpg"
+        };
+
+        String[] titulos = {
+                "El coco",
+                "El hombre de traje negro",
+                "El umbral de la noche",
+                "Harry Potter y la piedra filosofal",
+                "La odisea",
+                "Orgullo y prejuicio",
+                "Popsy"
+        };
+
+        audiolibros = new ArrayList<>();
+        for(int i = 0; i < titulos.length; i++){
+            Audiolibro a = new Audiolibro(i, titulos[i], i, "descripcion", portadas[i]);
+            audiolibros.add(a);
+        }
+
+        InfoAudiolibros.setTodosLosAudiolibros(audiolibros);
+    }
 }
+
+
