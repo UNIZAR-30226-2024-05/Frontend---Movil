6a46d44228964105052df7d0921b4d7fa9498da4
840091 <840091@unizar.es>
2024-05-13 17:53:38 +0200

    Mostradas reseñas publicas

--- NarrativesMobile/app/src/main/java/com/example/narratives/activities/ResenasActivity.java
+++ NarrativesMobile/app/src/main/java/com/example/narratives/activities/ResenasActivity.java
@@ -1,5 +1,56 @@
 package com.example.narratives.activities;
 
-public class ResenasActivity {
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.transition.TransitionSet;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.narratives.R;
+import com.example.narratives.adaptadores.AudiolibrosColeccionAdapter;
+import com.example.narratives.adaptadores.ColeccionesAdapter;
+import com.example.narratives.adaptadores.ResenasAdapter;
+import com.example.narratives.informacion.InfoMiPerfil;
+import com.example.narratives.peticiones.audiolibros.especifico.GenericReview;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class ResenasActivity extends AppCompatActivity {
+
+    ArrayList<GenericReview> resenasPublicasList = InfoLibroActivity.audiolibroActual.getPublicReviews();
+    private ColeccionesAdapter resenasAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS);
+        getWindow().setExitTransition(new TransitionSet());
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resenas);
+
+        TextView textViewTituloLibroResenas = findViewById(R.id.textViewTituloLibroResenas);
+        textViewTituloLibroResenas.setText(InfoLibroActivity.audiolibroActual.getAudiolibro().getTitulo());
+
+        TextView textViewNingunaResena = findViewById(R.id.textViewNingunaResena);
+        if (resenasPublicasList == null || resenasPublicasList.isEmpty()) {
+            textViewNingunaResena.setVisibility(View.VISIBLE);
+        } else {
+            textViewNingunaResena.setVisibility(View.GONE);
+        }
+
+        ResenasAdapter resenasAdapter = new ResenasAdapter(this, R.layout.item_lista_resenas, resenasPublicasList);
+
+        ListView listViewListaResenas = findViewById(R.id.listViewListaResenas);
+        listViewListaResenas.setAdapter(resenasAdapter);
+
+        FloatingActionButton botonAnadir = findViewById(R.id.fabNuevaResena);
+    }
 }
---
+++ NarrativesMobile/app/src/main/java/com/example/narratives/adaptadores/ResenasAdapter.java
@@ -0,0 +1,57 @@
+package com.example.narratives.adaptadores;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+
+import com.example.narratives.R;
+import com.example.narratives.peticiones.audiolibros.especifico.GenericReview;
+
+import java.util.List;
+
+public class ResenasAdapter extends ArrayAdapter<GenericReview> {
+    private Context context;
+    private int resourceLayout;
+    private List<GenericReview> resenasList;
+
+    public ResenasAdapter(@NonNull Context context, int resourceLayout, @NonNull List<GenericReview> resenasList) {
+        super(context, 0, resenasList);
+        this.context = context;
+        this.resourceLayout = resourceLayout;
+        this.resenasList = resenasList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(resourceLayout, null);
+        }
+
+        TextView textViewUsernameResenas = view.findViewById(R.id.textViewUsernameResenas);
+        textViewUsernameResenas.setText(resenasList.get(position).getUsername());
+
+        CardView cardView = view.findViewById(R.id.cardView);
+        if (resenasList.get(position).getComentario() == null | resenasList.get(position).getComentario().isEmpty()) {
+            cardView.setVisibility(View.GONE);
+        } else {
+            cardView.setVisibility(View.VISIBLE);
+            TextView textViewComentarioResena = cardView.findViewById(R.id.textViewComentarioResena);
+            textViewComentarioResena.setText(resenasList.get(position).getComentario());
+        }
+
+        RatingBar ratingBarListaResenas = view.findViewById(R.id.ratingBarListaResenas);
+        ratingBarListaResenas.setRating((float) resenasList.get(position).getPuntuacion());
+
+        return view;
+    }
+}
